8404f885234aa789f1a69016d76682c8771ba87d
Hayden Carlson <haydev@Haydev.lan>
2022-11-05 11:18:01 -0500

    started navbar, started custom logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import {faBars, FaTimes} from 'react-icons/fa'
+import Logo from '../assets/logo1.png'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='fixed w-full h-[80px] flex justify-between items center px-4 bg-[#0D1B2A] text-gray-300'>
+        <img src={Logo} alt="Logo Image" style={{width: '50px'}} />
+    </div>
   )
 }
 
